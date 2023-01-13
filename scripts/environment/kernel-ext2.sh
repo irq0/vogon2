a3355d8876ce8b93136376f825bc5a16c171cfa0
Marcel Lauhoff <ml@irq0.org>
2023-01-13 19:16:04 +0100

    remove vogon shell scripts

--- scripts/environment/kernel-ext2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source "$(dirname "$0")/../vogon.sh"
-
-vogon_testenv_harddisk "$VOGON_BLOCKDEV"
--- scripts/environment/s3gw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source "$(dirname "$0")/../vogon.sh"
-
-vogon_testenv_harddisk "$(readlink -f "$VOGON_SFS_BLOCKDEV")"
-vogon_testenv "s3gw-version" "$("$VOGON_S3GW_BIN" --version)"
